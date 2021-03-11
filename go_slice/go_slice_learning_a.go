d50dac4e4494cc0bd2073d5485ddd461b04bfd60
miracle_krato <924045318@qq.com>
2021-03-11 16:12:08 +0800

    Update go_slice_learning_a.go

--- go_slice/go_slice_learning_a.go
+++ go_slice/go_slice_learning_a.go
@@ -4,14 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	foo := make([]int, 5)
+	foo := make([]int, 5, 10)
 	foo[3] = 42
 	foo[4] = 100
 
 	bar := foo[1:4]
 	bar[1] = 90
-	fmt.Println(foo)
+	fmt.Println(foo,len(foo),cap(foo))
+    
 	a := make([]int, 32)
+	fmt.Println(cap(a),len(a))
 	b := a[1:16]
 	a = append(a,1)
 	a[2] = 42
