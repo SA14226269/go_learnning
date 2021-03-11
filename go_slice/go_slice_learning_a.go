4355473e1b284a55120f24d7012f1a9d7e07a83b
miracle_krato <924045318@qq.com>
2021-03-11 16:03:31 +0800

    Update go_slice_learning_a.go

--- go_slice/go_slice_learning_a.go
+++ go_slice/go_slice_learning_a.go
@@ -7,7 +7,7 @@ func main() {
 	foo := make([]int, 5)
 	foo[3] = 42
 	foo[4] = 100
-	
+
 	bar := foo[1:4]
 	bar[1] = 90
 	fmt.Println(foo)
@@ -18,4 +18,8 @@ func main() {
 	fmt.Println(a,b)
 	fmt.Println(cap(a),len(a))
 	fmt.Println(cap(b),len(b))
+
+	test1 := make([]int, 1024)
+	test1 = append(test1, 1) //1.25 * cap(1024)
+	fmt.Println(cap(test1),len(test1))
 }
